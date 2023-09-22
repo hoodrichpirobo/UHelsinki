841fb9be05a24333d15338cf28fd1f28e1e46c64
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-09-22 10:16:56 +0200

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Give a year: ");
+        int number = Integer.valueOf(scan.nextLine());
+
+        if (number % 4 != 0) {
+            System.out.println("The year is not a leap year.");
+        } else if (number % 100 == 0 && number % 400 != 0) {
+            System.out.println("The year is not a leap year.");
+        } else {
+            System.out.println("The year is a leap year.");
+        }
+    }
+}
