b287ae366cf018340738915e985fa9417f02a32b
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-09-26 10:14:03 +0200

    Create NumberAndSumOfNumbers.java

---
+++ NumberAndSumOfNumbers.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class NumberAndSumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        int sum = 0;
+        
+        while(true){
+            System.out.println("Give a number: ");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if(number == 0){
+                break;
+            }
+            
+            count += 1;
+            sum += number;
+        }
+        
+        System.out.println("Number of numbers: " + count);
+        System.out.println("Sum of the numbers: " + sum);
+    }
+}
