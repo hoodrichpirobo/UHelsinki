3191ea48786e5d57cb4933b99f7597bf71d5cc24
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-09-27 10:16:23 +0200

    Create FromWhereToWhere.java

---
+++ FromWhereToWhere.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class FromWhereToWhere {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Where to? ");
+        int end = Integer.valueOf(scanner.nextLine());
+        System.out.println("Where from? ");
+        
+        for (int start = Integer.valueOf(scanner.nextLine());start <= end; start++){
+            System.out.println(start);
+        }
+    }
+}
