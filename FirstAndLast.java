4edf8780f2d821c9b7116b9fd5835ceade34b008
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-11-07 19:42:45 +0100

    Create FirstAndLast.java

---
+++ FirstAndLast.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FirstAndLast {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int i = 0;
+        ArrayList<String> list = new ArrayList<>();
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+
+            list.add(input);
+            i++;
+        }
+        
+        System.out.println(list.get(0));
+        System.out.println(list.get(i-1));
+    }
+}
