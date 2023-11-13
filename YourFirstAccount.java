9bbc254e832c8dc394dd92e4e55c62089da05292
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-11-13 14:21:39 +0100

    Create YourFirstAccount.java

---
+++ YourFirstAccount.java
@@ -0,0 +1,12 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        
+        Account first = new Account("me", 100);
+        first.deposit(20);
+        System.out.println(first);
+    }
+}
