6ab9fca4b18c1932d42ecd7fdf00b710fe8e63a7
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-11-14 06:29:33 +0100

    Create Multiplier.java

---
+++ Multiplier.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cux27
+ */
+public class Multiplier {
+    
+    private int num;
+    
+    public Multiplier(int number){
+        this.num = number;
+    }
+    
+    public int multiply(int number){
+        return number *= this.num;
+    }
+    
+}
