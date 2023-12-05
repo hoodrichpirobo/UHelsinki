841460e7ecbac80f4d6b1cf1316ec2eda8f887d7
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-12-05 09:21:44 +0100

    Create NumberOfStrings.java

---
+++ NumberOfStrings.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class NumberOfStrings {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = 0;
+        
+        while(true){
+            String str = scanner.nextLine();
+            
+            if(str.equals("end")){
+                break;
+            }
+            
+            i++;
+        }
+        
+        System.out.println(i);
+
+    }
+}
