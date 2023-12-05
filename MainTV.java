41b766a9b2d02ef0be27cc36774de5b575a7cb3c
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-12-05 08:33:03 +0100

    Create MainTV.java

---
+++ MainTV.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // implement here your program that uses the TelevisionProgram class
+
+        ArrayList<TelevisionProgram> programs = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        
+        while(true){
+            System.out.println("Name: ");
+            String name = scanner.nextLine();
+            if(name.isEmpty()){
+                break;
+            }
+            
+            System.out.println("Duration: ");
+            int duration = scanner.nextInt();
+            scanner.nextLine();
+            programs.add(new TelevisionProgram(name, duration));
+        }
+        
+        System.out.println("Program's maximum duration? ");
+        
+        int max = scanner.nextInt();
+        for(TelevisionProgram show : programs){
+            if(show.getDuration()<= max){
+                System.out.println(show.getName() + ", "+ show.getDuration() + " minutes");
+            }
+        }
+
+    }
+}
