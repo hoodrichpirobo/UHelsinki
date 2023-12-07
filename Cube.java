957983582b45411e5c8f72fb96c647b1f1a4bd71
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-12-07 15:58:32 +0100

    Create Cube.java

---
+++ Cube.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cux27
+ */
+public class Cube {
+
+    int edgeLength;
+    
+    public Cube(int edgeLength){
+        this.edgeLength = edgeLength;
+    }
+    
+    public int volume(){
+        return this.edgeLength * this.edgeLength * this.edgeLength;
+    }
+    
+    @Override
+    public String toString(){
+        return "The length of the edge is " + this.edgeLength + " and the volume " + this.volume();
+    }
+    
+}
