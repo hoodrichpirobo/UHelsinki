90d3b4065c97e0fb486784082c4819883635210d
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2023-12-25 08:12:35 +0000

    Create Menu.java

---
+++ Menu.java
@@ -0,0 +1,29 @@
+
+import java.util.*;
+
+public class Menu {
+
+    private ArrayList<String> meals;
+
+    public Menu() {
+        this.meals = new ArrayList<>();
+    }
+
+    // implement the required methods here
+    
+    public void addMeal(String meal){
+        if(!(this.meals.contains(meal))){
+            this.meals.add(meal);
+        }
+    }
+    
+    public void printMeals(){
+        for(String each : this.meals){
+            System.out.println(each);
+        }
+    }
+    
+    public void clearMenu(){
+        this.meals.clear();
+    }
+}
