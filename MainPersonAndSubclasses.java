37c17af11f344e902928157940cb5ccdb6940dee
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-04-18 09:41:27 +0200

    Create MainPersonAndSubclasses.java

---
+++ MainPersonAndSubclasses.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class MainPersonAndSubclasses {
+
+    public static void main(String[] args) {
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+
+        printPersons(persons);
+}
+    
+    public static void printPersons(ArrayList<Person> persons){
+        for(Person each : persons){
+            System.out.println(each);
+        }
+    }
+
+}
