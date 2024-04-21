97e39ceb8323dbf38e0d1c39c1ff179a000b6922
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-04-21 17:13:14 +0200

    Create ProductWarehouse.java

---
+++ ProductWarehouse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cux27
+ */
+public class ProductWarehouse extends Warehouse{
+    private String productName;
+    public ProductWarehouse(String productName, double capacity){
+        super(capacity);
+        this.productName = productName;
+    }
+    public String getName(){
+        return this.productName;
+    }
+    public void setName(String newName){
+        this.productName = newName;
+    }
+    @Override
+    public String toString(){
+        return getName() + ": " + super.toString();
+    }
+}
