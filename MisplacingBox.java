bf333f3347ed224b1c00c04c3ed75b783208af59
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-04-21 19:54:31 +0200

    Create MisplacingBox.java

---
+++ MisplacingBox.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cux27
+ */
+public class MisplacingBox extends Box {
+    
+    public MisplacingBox(){
+        
+    }
+    
+    @Override
+    public void add(Item item){
+        
+    }
+    
+    @Override
+    public boolean isInBox(Item item){
+        return false;
+    }
+}
