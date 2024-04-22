c9164e97c6d5d1792d2baa81b0772f784558c431
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-04-22 12:53:38 +0200

    Create MainSets.java

---
+++ MainSets.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MainSets {
+
+    public static void main(String[] args) {
+        // You can test your method here
+
+    }
+
+    // implement the method returnSize here, which returns
+    // the number of elements in the set that it receives as a parameter.
+    public static int returnSize(Set set){
+        return set.size();
+    }
+}
