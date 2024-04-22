7a2b12bcc80d3ec7f643b858f9745d8007fa0f6f
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-04-22 11:18:08 +0200

    Create TripleTacoBox.java

---
+++ TripleTacoBox.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cux27
+ */
+public class TripleTacoBox implements TacoBox {
+    private int tacos;
+    
+    public TripleTacoBox(){
+        this.tacos = 3;
+    }
+    
+    @Override
+    public int tacosRemaining(){
+        return this.tacos;
+    }
+    
+    @Override
+    public void eat(){
+        if(this.tacos - 1 >= 0){
+            this.tacos--;
+        }
+    }
+}
