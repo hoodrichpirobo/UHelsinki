336b2659e78c28df78c4f76d051f092514a04315
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-04-22 12:49:29 +0200

    Create MainProgramIII.java

---
+++ MainProgramIII.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MainProgramIII {
+
+    public static void main(String[] args) {
+        // test your method here
+
+    }
+
+    // Implement here a method returnSize, which takes a Map-object as a parameter 
+    // and returns the size of the map object
+    public static int returnSize(Map map){
+        return map.size();
+    }
+}
