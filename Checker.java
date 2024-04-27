48427b270cd5974f3cc9638f313347bd8558483d
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-04-27 13:07:21 +0200

    Create Checker.java

---
+++ Checker.java
@@ -0,0 +1,23 @@
+public class Checker {
+    private String daysOfWeek;
+    private String vowels;
+    private String time;
+    
+    public Checker(){
+        this.daysOfWeek = "(mon|tue|wed|thu|fri|sat|sun)";
+        this.vowels = "[aeiou]*";
+        this.time = "([01]?[0-9]|2[0-3]):([0-5]?[0-9]):([0-5]?[0-9])";
+    }
+    
+    public boolean isDayOfWeek(String string){
+        return string.matches(this.daysOfWeek);
+    }
+    
+    public boolean allVowels(String string){
+        return string.matches(this.vowels);
+    }
+    
+    public boolean timeOfDay(String string){
+        return string.matches(this.time);
+    }
+}
