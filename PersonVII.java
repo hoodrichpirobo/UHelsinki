ad797b72b80f953982e6e4a41e82296219264d4c
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-04-28 16:34:55 +0200

    Create PersonVII.java

---
+++ PersonVII.java
@@ -0,0 +1,18 @@
+public class Person implements Saveable {
+    private String name, address;
+    
+    @Override
+    public void save(){
+        
+    }
+    
+    @Override
+    public void delete(){
+        
+    }
+    
+    @Override
+    public void load(String address){
+        
+    }
+}
