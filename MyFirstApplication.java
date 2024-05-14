fbb992e6a74d815bc0f46ec44f9ca658ccc222ba
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-05-14 12:36:54 +0200

    Create MyFirstApplication.java

---
+++ MyFirstApplication.java
@@ -0,0 +1,30 @@
+// ______   __  __   __  __    
+///\  ___\ /\ \/\ \ /\_\_\_\   
+//\ \ \____\ \ \_\ \\/_/\_\/_  
+// \ \_____\\ \_____\ /\_\/\_\
+//  \/_____/ \/_____/ \/_/\/_/
+
+package myFirstApplication;
+
+import javafx.application.*;
+import javafx.stage.*;
+
+public class MyFirstApplication extends Application {
+    
+
+    public static void main(String[] args) {
+        launch(MyFirstApplication.class);
+    }
+    
+    @Override
+    public void start(Stage window){
+        window.setTitle("My first application");
+        window.show();
+    }
+}
+
+//keep taking care of your goals, 
+//mental health, self care, recognize 
+//cognitive distortions and rebel against them!!
+//one step at a time, don't be afraid to 
+//ask 4 help or to fail at times.
