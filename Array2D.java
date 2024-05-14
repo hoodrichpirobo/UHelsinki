286d95f24cd206011a91c744a7be4a7958fc45ab
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-05-14 05:36:41 +0200

    Create Array2D.java

---
+++ Array2D.java
@@ -0,0 +1,36 @@
+// ______   __  __   __  __    
+///\  ___\ /\ \/\ \ /\_\_\_\   
+//\ \ \____\ \ \_\ \\/_/\_\/_  
+// \ \_____\\ \_____\ /\_\/\_\
+//  \/_____/ \/_____/ \/_/\/_/
+
+public class Program {
+
+    public static void main(String[] args) {
+        // Test your method here
+        int[][] matrix = {
+            {3, 2, 7, 6},
+            {2, 4, 1, 0},
+            {3, 2, 1, 0}
+        };
+
+        System.out.println(arrayAsString(matrix));     
+    }
+
+    public static String arrayAsString(int[][] array){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array[i].length; j++){
+                sb.append(array[i][j]);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+}
+
+//keep taking care of your goals, 
+//mental health, self care, recognize 
+//cognitive distortions and rebel against them!!
+//one step at a time, don't be afraid to 
+//ask 4 help or to fail at times.
