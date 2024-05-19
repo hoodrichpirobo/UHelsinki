d36f5de2beabf31910b222e43766420abe2730f1
CUX "INDIO" PRADA Aka Ramona Vega <74674131+hoodrichpirobo@users.noreply.github.com>
2024-05-19 19:21:01 +0200

    i'm disgusted by multimedia in programs

---
+++ CollageApplication.java
@@ -0,0 +1,66 @@
+package collage;
+
+// ______   __  __   __  __    
+///\  ___\ /\ \/\ \ /\_\_\_\   
+//\ \ \____\ \ \_\ \\/_/\_\/_  
+// \ \_____\\ \_____\ /\_\/\_\
+//  \/_____/ \/_____/ \/_/\/_/
+
+import javafx.application.*;
+import javafx.scene.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;
+import javafx.stage.*;
+
+public class CollageApplication extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Image sourceImage = new Image("file:monalisa.png");
+
+        PixelReader imageReader = sourceImage.getPixelReader();
+
+        int width = (int) sourceImage.getWidth();
+        int height = (int) sourceImage.getHeight();
+
+        WritableImage targetImage = new WritableImage(width, height);
+        PixelWriter imageWriter = targetImage.getPixelWriter();
+
+        for (int y = 0; y < height; y += 2) {
+            for (int x = 0; x < width; x += 2) {
+                Color color = imageReader.getColor(x, y);
+                double red = 1.0 - color.getRed();
+                double green = 1.0 - color.getGreen();
+                double blue = 1.0 - color.getBlue();
+                double opacity = color.getOpacity();
+
+                Color newColor = new Color(red, green, blue, opacity);
+
+                imageWriter.setColor(x / 2, y / 2, newColor);
+                imageWriter.setColor(width / 2 + x / 2, y / 2, newColor);
+                imageWriter.setColor(x / 2, height / 2 + y / 2, newColor);
+                imageWriter.setColor(width / 2 + x / 2, height / 2 + y / 2, newColor);
+            }
+        }
+
+        ImageView imageView = new ImageView(targetImage);
+
+        Pane pane = new Pane();
+        pane.getChildren().add(imageView);
+
+        stage.setScene(new Scene(pane));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(CollageApplication.class);
+    }
+}
+
+//keep taking care of your goals, 
+//mental health, self care, recognize 
+//cognitive distortions and rebel against them!!
+//one step at a time, don't be afraid to 
+//ask 4 help or to fail at times.
